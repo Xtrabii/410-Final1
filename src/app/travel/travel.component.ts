c597f338d6ec733017408cdfc184ca2a8979350b
Xtrabii <tawanntun@gmail.com>
2024-11-22 22:56:46 +0700

    First

---
+++ src/app/travel/travel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-travel',
+  imports: [],
+  templateUrl: './travel.component.html',
+  styleUrl: './travel.component.css'
+})
+export class TravelComponent {
+
+}
